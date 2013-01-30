cae64f1a9702dc3f752f79cbc0253434a97bb3ec
Anthony Miller <anthonymiller@outlook.com>
2013-01-30 11:44:51 -0800

    More updates

--- PA1milleant.asm
+++ PA1milleant.asm
@@ -21,7 +21,7 @@ remainder:	.word 0
 text:	.space 100
 
 .text 
-#---greet--------#
+#-----greet------#
 li 	$v0, 4			#Call system to primt stings
 la	$a0, greet		#Load the Greet statment
 syscall				#Print the Greet
@@ -32,9 +32,9 @@ la	$a0, prompt1		#Load 1st prompt
 syscall				#Print first prompt
 
 li	$v0, 8			#Call system to obtain number
-li	$a1, 100
-la	$a0, num1		#max number = 100
-syscall				#prompt user for number
+li	$a1, 100		#max number = 100
+la	$a0, num1		#save entry to num1
+syscall
 
 li	$v0, 4
 la	$a0, num1
