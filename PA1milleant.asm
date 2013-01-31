099482743b06a70734750228a3a709785ea3b03f
Anthony Miller <anthonymiller@outlook.com>
2013-01-30 20:11:15 -0800

    Fix all issues and completed project

--- PA1milleant.asm
+++ PA1milleant.asm
@@ -5,7 +5,10 @@
 .data
 
 greet: 		.ascii "\t\tComputation by Anthony Miller\n\n" 
-       		.asciiz "This program takes two numbers and runs calcuations on them!\n\n"	
+       		.asciiz "This program takes two numbers and runs calcuations on them!\n\n"
+       		
+close:		.asciiz "Thank you for using Computation! Have a good day!\n"	
+
 prompt1: 	.asciiz "Enter a number: "
 prompt2: 	.asciiz "Enter a second number: "
 
@@ -13,14 +16,15 @@ equals:		.asciiz " = "
 plus:		.asciiz " + "
 minus:		.asciiz " - "
 divide:		.asciiz " / "
+times:		.asciiz " * "
 leftover:	.asciiz " % "
 newline:	.asciiz "\n"
 
-sum:		.word 0
-difference:	.word 0
-product:	.word 0
-quotient:	.word 0
-remainder:	.word 0
+sum:		.word	0
+difference:	.word	0
+product:	.word	0
+quotient:	.word	0
+remainder:	.word	0
 
 .text 
 #-----greet------#
@@ -63,7 +67,7 @@ mfhi	$t4			#Move remainder into t4
 sw	$t3, quotient		#Store quotient in memory
 sw	$t4, remainder		#Store remainder in memory
 
-#---Print Output---#
+#---Print Sum Output---#
 li	$v0, 4			#
 la	$a0, newline		#New line
 syscall				#
@@ -85,13 +89,123 @@ li	$v0, 4			#
 syscall				#
 
 li	$v0, 1			#
-la	$a0, sum		#
+lw	$a0, sum		#
+syscall
 
+#---Print Difference Output--#
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall				#
 
+li	$v0, 1			#
+move	$a0, $t1		#
+syscall 			#
 
+la	$a0, minus		#
+li	$v0, 4			#Print out Difference of two numbers.
+syscall				#
 
+li	$v0, 1			#
+move	$a0, $t2		#
+syscall				#
 
+la	$a0, equals		#
+li	$v0, 4			#
+syscall				#
 
+li	$v0, 1			#
+lw	$a0, difference		#
+syscall
+
+#--Print Product---#
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t1		#
+syscall 			#
+
+la	$a0, times		#
+li	$v0, 4			#Print out Product of two numbers.
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t2		#
+syscall				#
+
+la	$a0, equals		#
+li	$v0, 4			#
+syscall				#
+
+li	$v0, 1			#
+lw	$a0, product		#
+syscall
+
+#--Print Qoutient---#
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t1		#
+syscall 			#
+
+la	$a0, divide		#
+li	$v0, 4			#Print out Quotient of two numbers.
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t2		#
+syscall				#
+
+la	$a0, equals		#
+li	$v0, 4			#
+syscall				#
+
+lw	$a0, quotient
+li	$v0, 1			#
+syscall
+
+#----Print Remainder----#
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t1		#
+syscall 			#
+
+la	$a0, leftover		#
+li	$v0, 4			#Print out Remainder of two numbers.
+syscall				#
+
+li	$v0, 1			#
+move	$a0, $t2		#
+syscall				#
+
+la	$a0, equals		#
+li	$v0, 4			#
+syscall				#
+
+li	$v0, 1			#
+lw	$a0, remainder		#
+syscall
+
+#---Print Close----#
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall	
+
+li	$v0, 4			#
+la	$a0, newline		#New line
+syscall	
+
+li	$v0, 4
+la	$a0, close
+syscall
+
+#---Exit-----#
 
 li	$v0, 10
 syscall
