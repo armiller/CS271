b21c04eb0358d7b2601fd91471e8e7e5ab51c158
Anthony Miller <anthonymiller@outlook.com>
2013-02-08 21:33:39 -0800

    Added variables

--- assignment_2/PA2milleant.asm
+++ assignment_2/PA2milleant.asm
@@ -1,2 +1,18 @@
-#   Author: Anthony Miller
-#   
+#   Author:         Anthony Miller
+#   Date:           February 8, 2012
+#   Description:    Prints out the number of fibionaciee numbers 
+#                   a user inputs.
+
+.data
+
+greet:      .ascii  "\t\tFibinnoci numbers: By Anthony Miller\n\n"
+            .asciiz "\tThis program outpus fibinoccie numbers!\n"
+
+getnumber:  .asciiz "How many fiboniacci numbers do you want to see?: "
+getname:    .asciiz "\nPlease enter your name: "
+
+newline:    .asciiz "\n"
+
+name:       .space  
+
+
