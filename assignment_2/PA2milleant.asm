2c32dbee4020c184d87b657ba8af1bfc2d47aec8
Anthony Miller <anthonymiller@outlook.com>
2013-02-08 22:09:41 -0800

    Retrieve numbers from user

--- assignment_2/PA2milleant.asm
+++ assignment_2/PA2milleant.asm
@@ -5,14 +5,32 @@
 
 .data
 
-greet:      .ascii  "\t\tFibinnoci numbers: By Anthony Miller\n\n"
-            .asciiz "\tThis program outpus fibinoccie numbers!\n"
+greet:      .ascii      "\tFibinnoci numbers: By Anthony Miller\n\n"
+            .asciiz     "\tThis program outpus fibinoccie numbers!\n"
 
-getnumber:  .asciiz "How many fiboniacci numbers do you want to see?: "
-getname:    .asciiz "\nPlease enter your name: "
+getnumber:  .asciiz     "How many fiboniacci numbers do you want to see?: "
+getname:    .asciiz     "\nPlease enter your name: "
 
-newline:    .asciiz "\n"
+newline:    .asciiz     "\n"
 
-name:       .space  
+name:       .space      100  
+
+n:          .word       0
+
+.text
+
+li      $v0, 4          #Prepare system to print string
+la      $a0, greet      #Load greet string
+syscall                 #Print string
+
+li      $v0, 4          #Prepare system to print string
+la      $a0, getname    #Load get name string
+syscall                 #Print string
+
+li      $v0, 5          #Prepare system to read Int
+syscall                 #Collect Int from user
+sw      $v0, n          #Store number to memory 
 
 
+li      $v0, 10         #Prepare system to exit
+syscall                 #Exit
