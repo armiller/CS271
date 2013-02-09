91b583e6652c0aa374ec2d85f790951a2d501a65
Anthony Miller <anthonymiller@outlook.com>
2013-02-09 13:39:37 -0800

    Added user greet with name

--- assignment_2/PA2milleant.asm
+++ assignment_2/PA2milleant.asm
@@ -8,37 +8,57 @@
 greet:      .ascii      "\tFibinnoci numbers\n\n\tBy: Anthony Miller\n\n"
             .asciiz     "This program outpus fibinoccie numbers!\n"
 
+greetintro: .asciiz     "Hello " 
 getnumber:  .asciiz     "\nHow many fiboniacci numbers do you want to see?: "
 getname:    .asciiz     "\nPlease enter your name: "
 
 newline:    .asciiz     "\n"
 
-name:       .space      100  
+name:       .space      64  
 
 n:          .word       0
 
 .text
+#--------Greet---------------#
 
 li      $v0, 4          #Prepare system to print string
 la      $a0, greet      #Load greet string
 syscall                 #Print string
 
+#-------Get Name------------#
 li      $v0, 4          #Prepare system to print string
 la      $a0, getname    #Load get name string
 syscall                 #Print string
 
 li      $v0, 8          #Prepare system to read string
-li      $a1, 100        #Maximum string size
+li      $a1, 64         #Maximum string size
 la      $a0, name       #Store name into memory
 syscall                 #Get name
 
+#------Greet with name------#
+
+#"Hello"
+li      $v0, 4          #Prrepare system to print string
+la      $a0, greetintro #Print "Hello"
+syscall                 #Print
+
+#"Name"
+li      $v0, 4          #Prepare system to print string
+la      $a0, name       #Load user name
+syscall                 #print name
+
+#------Get Number-----------#
+
+#"How many numbers do you want to see?"
 li      $v0, 4          #Prepare system to print string
 la      $a0, getnumber  #Load getnumber string
 syscall                 #Print string
 
+#"Get number()" 
 li      $v0, 5          #Prepare system to read Int
 syscall                 #Collect Int from user
 sw      $v0, n          #Store number to memory 
+
 
 
 li      $v0, 10         #Prepare system to exit
