98ee9569c9179f4646520c73778fb0229a617d05
Anthony Miller <anthonymiller@outlook.com>
2013-02-09 16:53:30 -0800

    Add if statement to make sure number is between the valid range

--- assignment_2/PA2milleant.asm
+++ assignment_2/PA2milleant.asm
@@ -12,6 +12,8 @@ greetintro: .asciiz     "Hello "
 getnumber:  .asciiz     "\nHow many fiboniacci numbers do you want to see?: "
 getname:    .asciiz     "\nPlease enter your name: "
 
+notvalid:   .asciiz     "\nNumber is invalid, must be between 1-47"
+
 newline:    .asciiz     "\n"
 
 name:       .space      64  
@@ -63,22 +65,34 @@ syscall                 #print name
 #   Continue...    
 #
 
-notvalid:   
+getNumLoop:   
             #"How many numbers do you want to see?"
-            li      $v0, 4          #Prepare system to print string
-            la      $a0, getnumber  #Load getnumber string
-            syscall                 #Print string
+            li      $v0, 4              #Prepare system to print string
+            la      $a0, getnumber      #Load getnumber string
+            syscall                     #Print string
 
             #get_number() 
-            li      $v0, 5          #Prepare system to read Int
-            syscall                 #Collect Int from user
-            sw      $v0, n          #Store number to memory 
+            li      $v0, 5              #Prepare system to read Int
+            syscall                     #Collect Int from user
+            sw      $v0, n              #Store number to memory 
+            move    $t0, $v0            #Move number to get ready for if statement
+
+            blez    $t0, nvalid       # if (n <= 0 ) goto notvalid label
+            bgt     $t0, 47, nvalid   # if (n > 47) goto notvalid label 
+            j       endloop             # goto end if (n > 0 && n < 47)
+nvalid:   
+            
+            #"Number is invalid, must be between 1-47. 
+            li      $v0, 4              #Get system ready to print string
+            la      $a0, notvalid       #Load not valid string
+            syscall                     #Print String
+
+            j       getNumLoop          #Go back to num loop
 
 
+endloop:
 
-
-
-valid:
+#-------Computer Fibonacci numbers-------#
 
 
 
